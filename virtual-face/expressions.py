fae4be29e398560659693218cf185a265ad24ed7
seanmatabsAtpolymorph <sean@polymorph.co.za>
2025-05-13 00:01:29 +0200

    feat: better expressions

--- virtual-face/expressions.py
+++ virtual-face/expressions.py
@@ -228,4 +228,131 @@ class FacialExpressions:
             time.sleep(duration / steps)
         
         # Always end with the target expression
-        self.draw_expression(to_expression) 
+        self.draw_expression(to_expression)
+
+    # Rounder eye helpers
+    def round_eye(x: int, y: int) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        # 2x2 block plus diagonals for roundness
+        return [((x, y+1), (x+1, y+1)), ((x, y), (x+1, y)), ((x, y), (x, y+1)), ((x+1, y), (x+1, y+1)), ((x, y), (x+1, y+1)), ((x+1, y), (x, y+1))]
+    def round_x_eye(x: int, y: int) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        # X shape plus diagonals for roundness
+        return [((x, y), (x+1, y+1)), ((x+1, y), (x, y+1)), ((x, y), (x, y+1)), ((x+1, y), (x+1, y+1))]
+    def round_slant_eye_left(x: int, y: int) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        return [((x, y+1), (x+1, y)), ((x, y), (x, y+1)), ((x+1, y), (x+1, y+1))]
+    def round_slant_eye_right(x: int, y: int) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        return [((x, y), (x+1, y+1)), ((x, y), (x, y+1)), ((x+1, y), (x+1, y+1))]
+    def round_closed_eye(x: int, y: int) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        return [((x, y+1), (x+1, y+1)), ((x, y), (x+1, y))]
+    def round_arc_eye(x: int, y: int) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        return [((x, y+1), (x+1, y)), ((x+1, y), (x+2, y+1)), ((x, y+1), (x+2, y+1))]
+
+    # Eye positions
+    left_eye_x, right_eye_x, eye_y = 1, 5, 1
+
+    # Eyes for each expression
+    round_left = round_eye(left_eye_x, eye_y)
+    round_right = round_eye(right_eye_x, eye_y)
+    round_x_left = round_x_eye(left_eye_x, eye_y)
+    round_x_right = round_x_eye(right_eye_x, eye_y)
+    round_slant_left = round_slant_eye_left(left_eye_x, eye_y)
+    round_slant_right = round_slant_eye_right(right_eye_x, eye_y)
+    round_closed_left = round_closed_eye(left_eye_x, eye_y)
+    round_closed_right = round_closed_eye(right_eye_x, eye_y)
+    round_arc_left = round_arc_eye(left_eye_x, eye_y)
+    round_arc_right = round_arc_eye(right_eye_x, eye_y)
+
+    # Mouth helpers (3 rows: 5, 6, 7)
+    def smile():
+        return [((1, 7), (2, 6)), ((2, 6), (3, 5)), ((3, 5), (4, 5)), ((4, 5), (5, 6)), ((5, 6), (6, 7))]
+    def frown():
+        return [((1, 5), (2, 6)), ((2, 6), (3, 7)), ((3, 7), (4, 7)), ((4, 7), (5, 6)), ((5, 6), (6, 5))]
+    def straight():
+        return [((1, 6), (6, 6)), ((2, 7), (5, 7))]
+    def open_mouth():
+        return [((2, 5), (2, 7)), ((5, 5), (5, 7)), ((2, 7), (5, 7)), ((2, 5), (5, 5))]
+    def gritted():
+        return [((1, 5), (6, 5)), ((1, 6), (6, 6)), ((1, 7), (6, 7)), ((1, 5), (1, 7)), ((3, 5), (3, 7)), ((5, 5), (5, 7))]
+    def o_mouth():
+        return [((3, 5), (4, 5)), ((3, 7), (4, 7)), ((3, 5), (3, 7)), ((4, 5), (4, 7)), ((2, 6), (5, 6))]
+    def wink_mouth():
+        return [((2, 7), (5, 7)), ((1, 6), (2, 7)), ((5, 7), (6, 6)), ((3, 6), (4, 6))]
+    def sleep_mouth():
+        return [((2, 7), (5, 7)), ((3, 6), (4, 6))]
+
+    self.expressions[ExpressionType.NEUTRAL] = Expression(
+        name="neutral",
+        points=[],
+        lines=[*round_left, *round_right, *straight()],
+        talk_lines={
+            TalkState.OPEN: [*round_left, *round_right, ((1, 6), (2, 6)), ((5, 6), (6, 6)), ((2, 7), (2, 7)), ((5, 7), (5, 7))],
+            TalkState.CLOSED: [*round_left, *round_right, *straight()],
+            TalkState.PARTIAL: [*round_left, *round_right, ((1, 6), (3, 6)), ((4, 6), (6, 6)), ((2, 7), (5, 7))]
+        },
+        duration=1.0
+    )
+    self.expressions[ExpressionType.HAPPY] = Expression(
+        name="happy",
+        points=[],
+        lines=[*round_left, *round_right, *smile()],
+        talk_lines={
+            TalkState.OPEN: [*round_left, *round_right, ((1, 7), (2, 7)), ((5, 7), (6, 7)), ((3, 6), (4, 6))],
+            TalkState.CLOSED: [*round_left, *round_right, *smile()],
+            TalkState.PARTIAL: [*round_left, *round_right, ((1, 7), (3, 6)), ((4, 6), (6, 7)), ((3, 7), (4, 7))]
+        },
+        duration=1.0
+    )
+    self.expressions[ExpressionType.SAD] = Expression(
+        name="sad",
+        points=[],
+        lines=[*round_x_left, *round_x_right, *frown()],
+        talk_lines={
+            TalkState.OPEN: [*round_x_left, *round_x_right, ((1, 5), (2, 5)), ((5, 5), (6, 5)), ((3, 6), (4, 6))],
+            TalkState.CLOSED: [*round_x_left, *round_x_right, *frown()],
+            TalkState.PARTIAL: [*round_x_left, *round_x_right, ((1, 5), (3, 6)), ((4, 6), (6, 5)), ((3, 5), (4, 5))]
+        },
+        duration=1.0
+    )
+    self.expressions[ExpressionType.ANGRY] = Expression(
+        name="angry",
+        points=[],
+        lines=[*round_slant_left, *round_slant_right, *gritted()],
+        talk_lines={
+            TalkState.OPEN: [*round_slant_left, *round_slant_right, ((1, 7), (2, 7)), ((5, 7), (6, 7)), ((3, 6), (4, 6))],
+            TalkState.CLOSED: [*round_slant_left, *round_slant_right, *gritted()],
+            TalkState.PARTIAL: [*round_slant_left, *round_slant_right, ((1, 7), (3, 7)), ((4, 7), (6, 7)), ((3, 6), (4, 6))]
+        },
+        duration=1.0
+    )
+    self.expressions[ExpressionType.SURPRISED] = Expression(
+        name="surprised",
+        points=[],
+        lines=[*round_left, *round_right, *o_mouth()],
+        talk_lines={
+            TalkState.OPEN: [*round_left, *round_right, ((3, 7), (4, 7)), ((2, 6), (2, 6)), ((5, 6), (5, 6))],
+            TalkState.CLOSED: [*round_left, *round_right, *o_mouth()],
+            TalkState.PARTIAL: [*round_left, *round_right, ((3, 5), (4, 5)), ((3, 7), (4, 7)), ((2, 6), (5, 6))]
+        },
+        duration=1.0
+    )
+    self.expressions[ExpressionType.WINK] = Expression(
+        name="wink",
+        points=[],
+        lines=[*round_closed_left, *round_right, *wink_mouth()],
+        talk_lines={
+            TalkState.OPEN: [*round_closed_left, *round_right, ((2, 7), (3, 7)), ((4, 7), (5, 7)), ((3, 6), (4, 6))],
+            TalkState.CLOSED: [*round_closed_left, *round_right, *wink_mouth()],
+            TalkState.PARTIAL: [*round_closed_left, *round_right, ((2, 7), (5, 7)), ((3, 6), (4, 6))]
+        },
+        duration=1.0
+    )
+    self.expressions[ExpressionType.SLEEPING] = Expression(
+        name="sleeping",
+        points=[],
+        lines=[*round_arc_left, *round_arc_right, *sleep_mouth()],
+        talk_lines={
+            TalkState.OPEN: [*round_arc_left, *round_arc_right, ((2, 7), (3, 7)), ((4, 7), (5, 7)), ((3, 6), (4, 6))],
+            TalkState.CLOSED: [*round_arc_left, *round_arc_right, *sleep_mouth()],
+            TalkState.PARTIAL: [*round_arc_left, *round_arc_right, ((2, 7), (5, 7)), ((3, 6), (4, 6))]
+        },
+        duration=1.0
+    ) 
